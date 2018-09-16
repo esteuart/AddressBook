3576e564a4561d41939c193f38826f2aafa53f48
esteuart <ews777@gmail.com>
2018-09-15 21:11:48 -0600

    Switched the name of the solution file. Added the backend web api project.

---
+++ AddressBook.Api/AddressBook.Api/Controllers/AddressController.cs
@@ -0,0 +1,50 @@
+﻿using AddressBook.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace AddressBook.Api.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class AddressController : ApiController
+    {
+        // GET: api/Address
+        public IEnumerable<Address> Get()
+        {
+            return new Address[] { new Address {
+                Id = 12,
+                Name = "Bob Barker",
+                Line1 = "123 State St",
+                City = "Los Angeles",
+                State = "CA",
+                Zip = "90210",
+                Phone = "800-515-9787"
+            } };
+        }
+
+        // GET: api/Address/5
+        public Address Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        // POST: api/Address
+        public void Post([FromBody]Address value)
+        {
+        }
+
+        // PUT: api/Address/5
+        public void Put(int id, [FromBody]Address value)
+        {
+        }
+
+        // DELETE: api/Address/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ AddressBook.Api/AddressBook.Api/Models/Address.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AddressBook.Api.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Phone { get; set; }
+    }
+}
